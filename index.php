af1717a56f348531ef6aa355828ffc0067b57da9
Nguyen Trong Dat <datbeo1999@gmail.com>
2019-08-06 15:11:26 +0700

    connect to db

--- index.php
+++ index.php
@@ -1,71 +1,40 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
-        
-        <title></title>
+        <meta charset="UTF-8">
+        <title>test</title>
     </head>
     <body>
-        <form method="post">
-            <table border="0" width="50%">
-            <tr>
-                <td>Name</td>
-                <td><input type="text" name="name"></td>
-            </tr>
-            <tr>
-                <td>Course</td>
-                <td><input name="course"></td>
-                <td>
-                    <select name="course">
-                        <option value="chim">chim</option>
-                        <option value="cho">cho</option>
-                        <option value="meo">meo</option>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <td>Birth</td>
-                <td><input type="date" name="dob"></td>
-            </tr>
-            <tr>
-                <td>Gender</td>
-                <td><input type="radio" name="gender" value="Male">buoi</td>
-                <td><input type="radio" name="gender" value="Female">buom</td>
-            </tr>
-            <tr>
-					<td>Favorites</td>
-					<td>
-						<input type="checkbox" name="porn" value="porn">Porn
-						<input type="checkbox" name="hentai" value="hentai">Hentai
-					</td>
-            </tr>
-            <tr>
-				<td></td>
-				<td><input type="submit" name="Register"></td>
-			</tr>
-                        
-            </table>               
-        </form>
-        <?php
-            $name=$_POST[name];
-            $course=$_POST[course];
-            $dob=$_POST[dob];
-            $gender = $_POST["gender"];
-	 $fav_book =$_POST["porn"];
-		$fav_car = $_POST["hentai"];
-		$fav = $fav_book . "," . $fav_car;
-        ?>
-        <h1>dkm cm luon day <?php echo $name?> for lon me cm <?php  echo $course?>;
-    </h1>
-    <ul>
-        <li><?php echo $dob?></li>
-        <li><?php echo $gender?></li>
-        <li><?php echo $fav?></li>
-    </ul>
-         
+    	<?php
+        	//$pdo = new PDO('pgsql:host=localhost;port=5432;dbname=GWCourses', 'postgres', 'datbeo09');
+        	//echo "fail!!!!!!";
+
+    	$db = parse_url(getenv("DATABASE_URL"));
+
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+        
+        $sql = "SELECT studentname, course FROM registercourse";
+		$stmt = $pdo->prepare($sql);
+		//Thiết lập kiểu dữ liệu trả về
+		$stmt->setFetchMode(PDO::FETCH_ASSOC);
+		$stmt->execute();
+		$resultSet = $stmt->fetchAll();
+				
+	?>
+	<ul>
+		<?php  
+			foreach ($resultSet as $row) {
+			echo '<li>' .
+				$row['studentname'] . ' --' . $row['course'] 
+				. '</li>';
+			}
+		?>
+	</ul>
     </body>
 </html>
